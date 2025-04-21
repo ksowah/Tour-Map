dff4d5e91cdcbe7553ec111668ac961384e3b610
ksowah <sowahkelvin640@gmail.com>
2025-04-21 13:28:34 +0000

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by macbook on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
