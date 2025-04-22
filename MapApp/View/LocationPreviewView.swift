93e3cc4d34d238c90bd42e7b0737ead6db0fa483
ksowah <sowahkelvin640@gmail.com>
2025-04-22 19:55:22 +0000

    first commit

---
+++ MapApp/View/LocationPreviewView.swift
@@ -0,0 +1,98 @@
+//
+//  LocationPreviewView.swift
+//  MapApp
+//
+//  Created by macbook on 21/04/2025.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    let location: Location
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            VStack(spacing: 16) {
+                    previewImage
+                    titleSection
+            }
+            VStack(spacing: 8) {
+                learnMoreButton
+                nextButton
+            }
+        }
+        .padding(20)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.ultraThinMaterial)
+                .offset(y:60)
+        )
+        .cornerRadius(10)
+    }
+}
+
+struct LocationPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            LocationPreviewView(location: LocationsDataService.locations.first!)
+                .padding()
+        }
+        .environmentObject(LocationsViewModel())
+    }
+}
+
+
+extension LocationPreviewView {
+    private var previewImage: some View {
+        ZStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(6)
+        .background(.white)
+        .cornerRadius(10)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(location.name)
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Text(location.cityName)
+                .font(.subheadline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var learnMoreButton: some View {
+        Button {
+            vm.sheetLocation = location
+        } label: {
+            Text("Learn More")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View {
+        Button {
+            vm.nextButtonPressed()
+        } label: {
+            Text("Next")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.bordered)
+    }
+}
---
+++ MapApp/View/LocationsListView.swift
@@ -0,0 +1,56 @@
+//
+//  LocationsListView.swift
+//  MapApp
+//
+//  Created by macbook on 21/04/2025.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    @EnvironmentObject private var vm: LocationsViewModel
+    var body: some View {
+        List {
+            ForEach(vm.locations) { location in
+                Button {
+                    vm.showNextLocation(location: location)
+                } label: {
+                    listItem(location: location)
+                }
+                .padding(.vertical, 4)
+                .listRowBackground(Color.clear)
+
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+}
+
+struct LocationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsListView()
+            .environmentObject(LocationsViewModel())
+    }
+}
+
+extension LocationsListView {
+    func listItem(location: Location) -> some View {
+        HStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 45, height: 45)
+                    .cornerRadius(10)
+            }
+            
+            VStack(alignment: .leading) {
+                Text(location.name)
+                    .font(.headline)
+                Text(location.cityName)
+                    .font(.subheadline)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
